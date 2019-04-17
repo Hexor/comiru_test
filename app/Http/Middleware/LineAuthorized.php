233f889306dc9e88a2175b1fdd6b23b4d5952c87
Hex Chang <cupsoda@gmail.com>
2019-04-18 00:28:57 +0800

    remove useless files

--- app/Http/Middleware/LineAuthorized.php
+++ app/Http/Middleware/LineAuthorized.php
@@ -20,8 +20,9 @@ class LineAuthorized
         try {
             $decoded = JWT::decode($request->line_token, env('LINE_CLIENT_SECRET'), ['HS256']);
             if ($decoded->exp <= time()) {
-                throw new Exception("expired", 401);
+                throw new Exception("expired");
             }
+            // 对于通过此中间件的请求, 在 request() 中附加上该用户的 line_id
             request()->merge(['line_id' => $decoded->sub]);
         } catch (\Exception $e) {
             return responseUnauthorized('Line 授权已经失效, 请重试');
